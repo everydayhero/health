dbbb17c83c2e11dbecbeb871357cf321ee91881c
Tim Cooper <coop@latrobest.com>
2015-03-26 18:57:07 +1000

    Default revision based on rails environment

--- lib/health/engine.rb
+++ lib/health/engine.rb
@@ -1,5 +1,20 @@
 module Health
   class Engine < ::Rails::Engine
     isolate_namespace Health
+
+    config.health = ActiveSupport::OrderedOptions.new
+    config.health.revision = ->(env) do
+      if env.production?
+        Rails.root.join("REVISION").read.chomp
+      else
+        `git rev-parse HEAD`.chomp
+      end
+    end
+
+    config.to_prepare { Engine.setup!(Rails.env) }
+
+    def self.setup!(env)
+      Health.revision = config.health.revision.call(env)
+    end
   end
 end
